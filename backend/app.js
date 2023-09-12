519a14708188718107bf9ee93fbb71a0e0ed1897
alber <albertalbez25@gmail.com>
2023-09-13 01:20:38 +0800

    commit

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://mwrsms-front.vercel.app',],
+  origin: ['https://mwrsms-front.vercel.app',],
   credentials: true
 }));
 
