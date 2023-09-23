a34e71c690c76f98edde4a1433916ac47fc073cc
alber <albertalbez25@gmail.com>
2023-09-23 16:58:32 +0800

    updated

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://testcode-sbpj.vercel.app',],
   credentials: true
 }));
 
